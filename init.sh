f1622a6307c7d41bee247d9a60ecc81a388b6d72
huncholane <huncho@ghostsystems.io>
2022-11-28 08:33:29 -0800

    builds from sh file

--- init.sh
+++ init.sh
@@ -1,5 +1,13 @@
-django-admin startprojectbackend
-cp init_files/backend/* backend
+echo "Installing django..."
+pip install django
+echo "Creating the django backend..."
+python3 -m django startproject backend
+echo "Copying initial files into backend..."
+cp init_files/backend/* backend/
 
-npx create-react-app
-cp init_files/frontend/* frontend
+echo "Creating the react frontend..."
+npx create-react-app frontend
+echo "Copying initial files into frontend..."
+cp init_files/frontend/* frontend/
+
+echo "Done."
